4ca1c5dc835018bec0f68b3dc3fe88a82b03ed38
Fatima Shahid <160149262+FatimaaShahid@users.noreply.github.com>
2025-07-28 01:24:54 +0500

    Added modify position feature

--- generator/views.py
+++ generator/views.py
@@ -178,6 +178,7 @@ def hex_to_rgb(value):
     lv = len(value)
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
+
 def generate_certificates(request):
     # Retrieve paths and configs
     csv_path = request.session.get('csv_path')
